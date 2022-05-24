efb2b9c650e28ef757777b452738edc26945636f
mossabdiae <mossabdiae@gmail.com>
2022-05-24 15:41:31 +0100

    refractored copystudent (not yet cleaned)

--- endpoint/src/copystudent.js
+++ endpoint/src/copystudent.js
@@ -14,67 +14,87 @@ export default (router, { services, getSchema, env }) => {
 	// The route gets called from the registration so it accepts the regitration id
 	router.get('/:pk', async (req, res, next) => {
 
-		// This function gets an element from a collection 
-		// needs: request (it gets id from it too), which collection
-		const get_user = async (req, collection) => {
-			const service = new ItemsService(collection, {
+		// initiating services
+		// Registration (used to get registred user and delete it later)
+		const reg_service = new ItemsService("registrations", {
+			accountability: req.accountability,
+			schema: req.schema,
+		})
+
+		// Students (used to create the student using data retrieved from registration)
+		const stud_service = new ItemsService("students", {
 				accountability: req.accountability,
 				schema: req.schema,
 			})
-			
-			const user = await service.readOne(req.params.pk, req.sanitizedQuery)
-				.catch(() => {
-					console.log("A bad err has happened");
-				})
-			if (user) {
-				return user
-			} else {
-				return false
-			}
 
-		}
+		// This function gets an element from a service
+		// needs: request (it gets id from it too), which collection
+		// const get_user = async (req, service) => {		
+		// 	const user = await service.readOne(req.params.pk, req.sanitizedQuery)
+		// 		.catch(() => {
+		// 			console.log("A bad err has happened");
+		// 		})
+		// 	if (user) {
+		// 		return user
+		// 	} else {
+		// 		return false
+		// 	}
+
+		// }
 
 		// This function creates an element
 		// needs: request, which collection , data
-		const create_user = async (req, collection, data) => {
-			const service = new ItemsService(collection, {
-				accountability: req.accountability,
-				schema: req.schema,
-			})
+		// const create_user = async (req, collection, data) => {
+		// 	const result = await service.createOne(data)
+		// 	return result
 
-			const result = await service.createOne(data)
-			return result
-
-		}
+		// }
 
 
-		// get user 
-		const regs_user = await get_user(req, "registrations")
-			// .catch((err) => {
-			// 	console.log(err);
-			// res.send("sorry an err happened")
-			// 	next(err)
-			// 	// return
-				
-			// })
+		try {
+			// Get user 
+			const regs_user = await reg_service.readOne(req.params.pk, req.sanitizedQuery)
 
-		console.log("############################################################");
-		console.log(regs_user);
-		if (regs_user && regs_user.program) {
 			const student_data = {
 				name: regs_user.name,
 				birthday: regs_user.birthday,
 				contact: regs_user.contact,
 				program: regs_user.program,
 			}
-			console.log(student_data);
-			const new_student = await create_user(req, "students", student_data);
-			if (new_student) {
+
+			if (student_data.program) {
+				const new_student = await stud_service.createOne(student_data)
 				res.redirect(`http://${HOST}:${PORT}/admin/content/students/${new_student}`)
+			} else {
+				res.send("Please specify the program in registration")
 			}
-		} else {
-				res.send("User not found or Program not specified")
+
+			// Create student
+			
+			// if (new_student) {
+			// 	res.redirect(`http://${HOST}:${PORT}/admin/content/students/${new_student}`)
+			// } else {
+			// 	res.send("User not found or Program not specified")
+			// }
+
+		} catch (err) {
+			console.log("A bad err has happened");
+			res.send("an error has happened")
 		}
+		// const regs_user = await get_user(req, "registrations")
+
+
+		// console.log("############################################################");
+		// console.log(regs_user);
+		// if (regs_user && regs_user.program) {
+		// 	const student_data = {
+		// 		name: regs_user.name,
+		// 		birthday: regs_user.birthday,
+		// 		contact: regs_user.contact,
+		// 		program: regs_user.program,
+		// 	}
+		// 	console.log(student_data);
+		// 	const new_student = await create_user(req, "students", student_data);
 
 	})
 };
