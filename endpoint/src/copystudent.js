08c69c50279132a89d89a51e8baef760d76fc5ab
mossabdiae <mossabdiae@gmail.com>
2022-05-24 15:55:57 +0100

    cleaned comments and added delete

--- endpoint/src/copystudent.js
+++ endpoint/src/copystudent.js
@@ -1,17 +1,9 @@
-// for testing purpose you can set the request to get, and create the user based on this json
-// other than that set service.createOne(req.body)
-
-// const new_student = {
-// 	name: "bibou"
-// }
-
-// You can access these 
 
 export default (router, { services, getSchema, env }) => {
 	const { ItemsService } = services
 	const {HOST, PORT} = env
 
-	// The route gets called from the registration so it accepts the regitration id
+	// The route gets called from the registration so it accepts the regitration id (pk)
 	router.get('/:pk', async (req, res, next) => {
 
 		// initiating services
@@ -27,29 +19,6 @@ export default (router, { services, getSchema, env }) => {
 				schema: req.schema,
 			})
 
-		// This function gets an element from a service
-		// needs: request (it gets id from it too), which collection
-		// const get_user = async (req, service) => {		
-		// 	const user = await service.readOne(req.params.pk, req.sanitizedQuery)
-		// 		.catch(() => {
-		// 			console.log("A bad err has happened");
-		// 		})
-		// 	if (user) {
-		// 		return user
-		// 	} else {
-		// 		return false
-		// 	}
-
-		// }
-
-		// This function creates an element
-		// needs: request, which collection , data
-		// const create_user = async (req, collection, data) => {
-		// 	const result = await service.createOne(data)
-		// 	return result
-
-		// }
-
 
 		try {
 			// Get user 
@@ -63,38 +32,23 @@ export default (router, { services, getSchema, env }) => {
 			}
 
 			if (student_data.program) {
+				// Create student
 				const new_student = await stud_service.createOne(student_data)
+
+				// Then delete registration entry
+				await reg_service.deleteOne(req.params.pk);
+
+				//  Redirect to newly created student
 				res.redirect(`http://${HOST}:${PORT}/admin/content/students/${new_student}`)
+
 			} else {
 				res.send("Please specify the program in registration")
 			}
-
-			// Create student
-			
-			// if (new_student) {
-			// 	res.redirect(`http://${HOST}:${PORT}/admin/content/students/${new_student}`)
-			// } else {
-			// 	res.send("User not found or Program not specified")
-			// }
 
 		} catch (err) {
 			console.log("A bad err has happened");
 			res.send("an error has happened")
 		}
-		// const regs_user = await get_user(req, "registrations")
-
-
-		// console.log("############################################################");
-		// console.log(regs_user);
-		// if (regs_user && regs_user.program) {
-		// 	const student_data = {
-		// 		name: regs_user.name,
-		// 		birthday: regs_user.birthday,
-		// 		contact: regs_user.contact,
-		// 		program: regs_user.program,
-		// 	}
-		// 	console.log(student_data);
-		// 	const new_student = await create_user(req, "students", student_data);
 
 	})
 };
